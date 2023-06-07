eab1f99e8de5e397db25d8676279f91ffa7bc4e0
alina <akupriianova99@gmail.com>
2023-06-07 23:21:55 +0300

    add en language

---
+++ src/locales/en.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    error: {
+      url: 'The link must be valid URL',
+      notOneOf: 'RSS already exists',
+      network: 'Poor network',
+      noEmpty: 'Must not be empty',
+      noRSS: 'The source does not contain valid RSS',
+    },
+    hint: {
+      success: 'RSS was uploaded successfully',
+    },
+  },
+};
